9a292fb9f470c693db2df28fcfbf85c180554b38
fawzy <fwzymohamed90@gmail.com>
2025-07-21 01:35:51 +0300

    add swagger for dto

--- src/main/java/com/trading/task_management/tasks/dto/TaskRequest.java
+++ src/main/java/com/trading/task_management/tasks/dto/TaskRequest.java
@@ -8,18 +8,18 @@ import java.time.LocalDate;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record TaskRequest(
-//    @Schema(description = "Title of the task", example = "Finish report")
-//    @NotBlank
+    @Schema(description = "Title of the task", example = "Finish report")
+    @NotBlank
     String title,
 
-//    @Schema(description = "Description of the task", example = "Complete the quarterly financial report")
+    @Schema(description = "Description of the task", example = "Complete the quarterly financial report")
     String description,
 
-//    @Schema(description = "Due date for the task", example = "2024-07-31")
-//    @FutureOrPresent
+    @Schema(description = "Due date for the task", example = "2024-07-31")
+    @FutureOrPresent
     LocalDate dueDate,
 
-//    @Schema(description = "Priority of the task", example = "HIGH")
-//    @NotNull
+    @Schema(description = "Priority of the task", example = "HIGH")
+    @NotNull
     String priority
 ) {} 
